1dfc8f76e58eb118426e7a28313ac2654df35f40
Lívia Nascimento <149019255+Livia-Nasc@users.noreply.github.com>
2024-02-22 19:09:49 -0300

    pessoa.py

--- pessoa.py
+++ pessoa.py
@@ -1,3 +1,5 @@
+# Crie uma classe que modele uma Pessoa, tendo como atributos nome, idade, peso e altura, e tendo como métodos envelhecer (idade recebe mais um a cada ano) e crescer (altura recebe 0,5 cm por ano até idade de 21 anos).
+
 class pessoa:
     def __init__(self, nome, idade, altura, peso, anos):
         self.nome = nome
